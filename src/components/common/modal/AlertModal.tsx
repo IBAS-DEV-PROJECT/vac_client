0b50312de191237d29b21cd9146f84564346e042
kimjihu24 <163487682+kimjihu24@users.noreply.github.com>
2026-08-09 19:08:51 +0900

    Update AlertModal.tsx

--- src/components/common/modal/AlertModal.tsx
+++ src/components/common/modal/AlertModal.tsx
@@ -1,3 +1,4 @@
+import { useId } from 'react'
 import Modal from './Modal'
 
 interface AlertModalProps {
@@ -13,14 +14,16 @@ function AlertModal({
   message,
   confirmText = '확인',
 }: AlertModalProps) {
+  const messageId = useId()
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} ariaLabelledBy={messageId}>
       <p className="mb-[14px] text-center text-[15px] font-bold leading-[1.4] text-[#201E1D">
         {message}
       </p>
       <button
         type="button"
         onClick={onClose}
+        id={messageId}
         className="h-12 w-full rounded-lg bg-[#3E2723] text-[15px] font-bold text-white"
       >
         {confirmText}
