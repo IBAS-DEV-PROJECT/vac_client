22a4ef81486ce63fdf46ebb40fb524094ca857a5
seohyeon0219 <seohyeonidaya@gmail.com>
2026-08-19 01:12:39 +0900

    refactor: eul 함수 korean 유틸로 분리 및 중복 제거

--- src/components/insights/InsightCard.tsx
+++ src/components/insights/InsightCard.tsx
@@ -1,10 +1,5 @@
 import { VALUE_COLORS, VALUE_LABELS, type ValueKey } from '@/constants/insights'
-
-function eul(word: string): string {
-  const code = word.charCodeAt(word.length - 1) - 0xac00
-  if (code < 0) return '을'
-  return code % 28 === 0 ? '를' : '을'
-}
+import { eul } from '@/utils/korean'
 
 interface InsightCardProps {
   topTopicLabel: string
--- src/components/insights/ValueTrendSection.tsx
+++ src/components/insights/ValueTrendSection.tsx
@@ -13,12 +13,7 @@ import {
   type TrendDataPoint,
   type ValueKey,
 } from '@/constants/insights'
-
-function eul(word: string): string {
-  const code = word.charCodeAt(word.length - 1) - 0xac00
-  if (code < 0) return '을'
-  return code % 28 === 0 ? '를' : '을'
-}
+import { eul } from '@/utils/korean'
 
 interface ValueTrendSectionProps {
   data: TrendDataPoint[]
