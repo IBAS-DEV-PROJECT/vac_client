135b8905179eb84ab189f17dbede14007df52713
sangwon <leesangwon83@naver.com>
2026-08-20 04:10:53 +0900

    fixed:온보딩 페이지 컴포넌트 OnboardingHeader의 코드를 map을 사용해서 변경

--- src/components/onboarding/OnboardingHeader.tsx
+++ src/components/onboarding/OnboardingHeader.tsx
@@ -1,38 +1,14 @@
 export default function OnboardingHeader(props: { pageNumber:number; }) {
-  {/*colored block numbers change by pageNumber */}
-  switch(props.pageNumber){
-    case 1: 
-    {/*on pageNumber == 1*/}
-      return (
-        <header className="h-[31px] grow-0 px-[28px]">
-          <div className="flex flex-row items-end h-[100%]">
-            <div className="h-[3px] w-[32px] bg-[#3E2723] mr-[8px]"/>
-            <div className="h-[3px] w-[32px] bg-[#DDF0FA] mr-[8px]"/>
-            <div className="h-[3px] w-[32px] bg-[#DDF0FA] mr-[8px]"/>
-          </div>
-        </header>
-      );
-    case 2:
-    {/*on pageNumber == 2*/}
-      return (
-        <header className="h-[31px] grow-0 px-[28px]">
-          <div className="flex flex-row items-end h-[100%]">
-            <div className="h-[3px] w-[32px] bg-[#3E2723] mr-[8px]"/>
-            <div className="h-[3px] w-[32px] bg-[#3E2723] mr-[8px]"/>
-            <div className="h-[3px] w-[32px] bg-[#DDF0FA] mr-[8px]"/>
-          </div>
-        </header>
-      );
-    case 3:
-    {/*on pageNumber == 3*/}
-      return (
-        <header className="h-[31px] grow-0 px-[28px]">
-          <div className="flex flex-row items-end h-[100%]">
-            <div className="h-[3px] w-[32px] bg-[#3E2723] mr-[8px]"/>
-            <div className="h-[3px] w-[32px] bg-[#3E2723] mr-[8px]"/>
-            <div className="h-[3px] w-[32px] bg-[#3E2723] mr-[8px]"/>
-          </div>
-        </header>
-      );
-  }
+  return (
+    <header className="h-[31px] px-7 flex items-end gap-2">
+      {[1, 2, 3].map((step) => (
+        <div
+          key={step}
+          className={`h-[3px] w-8 ${
+            step <= props.pageNumber ? 'bg-[#3E2723]' : 'bg-[#DDF0FA]'
+          }`}
+        />
+      ))}
+    </header>
+  )
 }
